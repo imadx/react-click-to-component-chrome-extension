3b60952ebb41855edb90cd5222e64f740b61a023
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2024-06-23 15:07:23 +0530

    add workaround for nest projects with turbo

--- content-script.js
+++ content-script.js
@@ -39,8 +39,24 @@
   const handleClick = (event) => {
     if (!event.altKey) return;
 
-    const pathToSource = getPathToSourceFromTarget(event.target);
+    let pathToSource = getPathToSourceFromTarget(event.target);
     if (!pathToSource) return;
+
+    const localStorageKey = `clickToReact-projectPath-${window.location.host}`;
+    if (pathToSource.includes("file://./")) {
+      let projectPath = localStorage.getItem(localStorageKey);
+      if (!projectPath) {
+        projectPath = prompt(
+          "Enter absolute path to the project root directory. This will be saved in localStorage if you want to modify it later.",
+        );
+        localStorage.setItem(localStorageKey, projectPath);
+      }
+
+      pathToSource = pathToSource.replace(
+        "file://./",
+        `file://${projectPath}/`,
+      );
+    }
     window.location.assign(pathToSource);
   };
 
